e5b69923a887d02d1cfe65c72f26f3a4e4e35606
starkstar <wyhuang1980@gmail.com>
2015-12-21 13:47:07 +0800

    add views/ok.jsp

--- src/main/java/com/bbq/task/handler/UserHandler.java
+++ src/main/java/com/bbq/task/handler/UserHandler.java
@@ -1,8 +1,15 @@
 package com.bbq.task.handler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserHandler {
+	
+	@RequestMapping(value="/index")
+	public String index(){
+		
+		return "ok";
+	}
 
 }
